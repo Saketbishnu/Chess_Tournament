9abcb976a484ab7827afc4cb7bb134361e34099c
Saket Bishnu <saketbsn@gmail.com>
2026-07-09 03:17:42 +0530

    created player management form

---
+++ src/routes/players/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+let name = $state("");
+let age = $state("");
+let rating = $state("");
+let country = $state("");
+</script>
+
+<h1>Player Management</h1>
+
+<form>
+    <div>
+        <label>Name</label><br>
+        <input bind:value={name} type="text" />
+    </div>
+
+    <br>
+
+    <div>
+        <label>Age</label><br>
+        <input bind:value={age} type="number" />
+    </div>
+
+    <br>
+
+    <div>
+        <label>Rating</label><br>
+        <input bind:value={rating} type="number" />
+    </div>
+
+    <br>
+
+    <div>
+        <label>Country</label><br>
+        <input bind:value={country} type="text" />
+    </div>
+
+    <br>
+
+    <button>Add Player</button>
+</form>
